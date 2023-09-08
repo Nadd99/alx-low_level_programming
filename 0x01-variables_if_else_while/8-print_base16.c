ceaae26abf4981abb59b2739fe803338a73bff6e
nad <its.nadd99@gmail.com>
2023-09-08 23:57:31 +0200

    commit changes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int nn;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+	}
+	for (nn = 97 ; n <= 102 ; nn++)
+	{
+		putchar(nn);
+	}
+	putchar('\n');
+	return (0);
+}
