2d0cd7dd2c99fc5a75b99d0e50c873f636ec3d5a
nad <its.nadd99@gmail.com>
2023-09-08 23:42:31 +0200

    commit changes

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
