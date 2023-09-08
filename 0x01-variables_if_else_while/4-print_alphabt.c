653eb46f42794474b57e915d416cb48cf6345bd6
nad <its.nadd99@gmail.com>
2023-09-08 23:27:52 +0200

    commit changes

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,23 @@
-i
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int ch = 97;
+
+	while (ch <= 122)
+	{
+		if (ch == 101 || ch == 113)
+		{
+			ch++;
+			continue;
+		}
+		putchar(ch);
+		ch++;
+	}
+	printf("\n");
+	return (0);
+}
