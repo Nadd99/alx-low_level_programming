473573f66eaac30f8f921923ca0c4e923a914c22
nad <its.nadd99@gmail.com>
2023-09-08 03:07:14 +0200

    commit changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /*
- * main - entry
+ * main - Entry point
  *
- * return: 0 (successful)
+ * Return: 0 (successful)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
