2afefeb42cd29f146ebb18a3025f513ae1734958
nad <its.nadd99@gmail.com>
2023-09-08 22:20:48 +0200

    commit changes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 	dig = n % 10;
 	if (dig > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, dig);
+		printf("Last digit of %d is %d and is greater than 5 ", n, dig);
 	}
 	else if (dig == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, dig);
+		printf("Last digit of %d is %d and is 0 ", n, dig);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, dig);
+		printf("Last digit of %d is %d and is less than 6 and not 0 ", n, dig);
 	}
 	return (0);
 }
