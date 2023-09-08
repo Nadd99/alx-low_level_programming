3ce7a52d87561168a521f6b85555098076f91441
nad <its.nadd99@gmail.com>
2023-09-08 04:28:47 +0200

    commit changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,16 @@
 /**
  * main - entry
  *
+ * Description: size of types
+ *
  * Return: 0 (success)
  */
 int main(void)
 {
-	printf("Size of a char: %lu bytes\n", sizeof(char));
-	printf("size of a int: %lu bytes\n", sizeof(int));
-	printf("size of a long int: %lu bytes\n", sizeof(long int));
-	printf("size of a long long int: %lu bytes\n", sizeof(long long int));
-	printf("size of a float: %lu bytes", sizeof(float));
+	printf("Size of a char: %u bytes\n", sizeof(char));
+	printf("size of a int: %u bytes\n", sizeof(int));
+	printf("size of a long int: %u bytes\n", sizeof(long int));
+	printf("size of a long long int: %u bytes\n", sizeof(long long int));
+	printf("size of a float: %u bytes", sizeof(float));
 	return (0);
 }
