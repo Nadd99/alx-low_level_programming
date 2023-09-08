439e19490cb2d81572d6e2a6e111949cebf7a029
nad <its.nadd99@gmail.com>
2023-09-08 23:03:54 +0200

    commit changes

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return:
+ * 0 (success)
+ */
+int main(void)
+{
+	char up;
+	char low;
+
+	for (up = 97 ; up <= 122 ; up++)
+	{
+		putchar(up);
+	}
+	for (low = 65 ; low <= 90 ; low++)
+	{
+		putchar(low);
+	}
+	printf("\n");
+}
