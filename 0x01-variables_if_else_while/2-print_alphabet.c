ecc7db68b3c3174f65b927c45a007280e7fb28d3
nad <its.nadd99@gmail.com>
2023-09-08 22:42:07 +0200

    commit changes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = "A" ; chr <= "Z" ; chr++)
+	{
+		putchar(chr);
+	}
+	return (0);
+}
