b9526968f50ff9c5479514ac8665883fb7ae384c
nad <its.nadd99@gmail.com>
2023-09-08 02:50:55 +0200

    commit changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main
+ * main - entry 
  *
- * return
+ * return: 0 
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+		return (0);
 }
