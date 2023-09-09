d6a750ddfaa720bb2deddb4c1ae0af1c5b90d234
nad <its.nadd99@gmail.com>
2023-09-09 03:18:41 +0200

    commit changes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,16 +12,16 @@ int main(void)
 	for (n = 48 ; n <= 57 ; n++)
 	for (m = 48 ; m <= 57 ; m++)
 	{
+		putchar(n);
+		putchar(m);
+		putchar(',');
+		putchar(' ');
 		if (n == m)
 		{
 			n++;
 			m++;
 			continue;
 		}
-		putchar(n);
-		putchar(m);
-		putchar(',');
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
