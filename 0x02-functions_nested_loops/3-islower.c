0e2c163cf55715bcbcc8c500d11345182d305d1c
nad <its.nadd99@gmail.com>
2023-09-12 19:21:02 +0200

    commit changes

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include <main.h>
+/**
+ * _islower - entry check lower case
+ * @c: charecter to be checked
+ * Return: 1 if lower case, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
