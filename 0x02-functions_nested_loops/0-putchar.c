93b892315f8b257322e80174f8ffb9a4548855f9
nad <its.nadd99@gmail.com>
2023-09-12 18:25:43 +0200

    commit changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - entry to a program code
+ *
+ * Discription: a function
  *
  * Return: 0
  */
 int main(void)
 {
 	printf("_putchar\n");
+	return (0);
 }
