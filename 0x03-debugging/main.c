f14648377b9e79da1d2d00a4679a27e8e0dca970
nad <its.nadd99@gmail.com>
2023-09-14 02:33:00 +0200

    commit changes

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -7,10 +7,9 @@ i#include "main.h"
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+	i = 98;
+	positive_or_negative(i);
+	return (0);
 }
