d2f91ddb4da1a74400d54de4f6a1dc6a86d8ec92
nad <its.nadd99@gmail.com>
2023-09-16 14:14:48 +0200

    commit changes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,37 +6,21 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
 	if (n <= 98)
 	{
 		for (; n <= 98; n++)
 		{
 			if (n == 98)
 			{
-				printf("%d", n);
-				printf("\n");
+				_putchar('n');
+				_putchar('\n');
 				break;
 			}
 			else
 			{
-				printf("%d, ", n);
-			}
-		}
-	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
+				_putchar('n');
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 	}
