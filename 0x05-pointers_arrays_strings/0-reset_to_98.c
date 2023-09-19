7c42c8b8287659572780608c2d1114335967674f
nad <its.nadd99@gmail.com>
2023-09-19 21:58:24 +0200

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98(int *n) - takes a pointer to int and updates its value to 98
+ * @n: pointer to n
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
