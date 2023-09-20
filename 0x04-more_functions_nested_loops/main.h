3e9aeb01d61175e232c57b0267408f62231c7769
nad <its.nadd99@gmail.com>
2023-09-20 14:45:50 +0200

    commit changes

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN-H
+#define MAIN-H
+#include <stdio.h>
+
 int _isdigit(int c);
 int _isupper(int c);
 int _putchar(int c);
@@ -10,3 +14,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif
