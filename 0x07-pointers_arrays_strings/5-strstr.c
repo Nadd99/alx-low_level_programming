6632decf54acbee992cbf0abbe36e967e681117c
nad <its.nadd99@gmail.com>
2023-09-26 01:44:30 +0200

    created strstr function

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: the main string to be examined
+ * @needle: the sub-string to be searched in string
+ * Return: a pointer to the first charecter found, else NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int n = 0, m = 0;
+
+	while (haystack[n])
+	{
+		while (needle[m])
+		{
+			if (haystack[n + m] != needle[m])
+			{
+				break;
+			}
+
+			m++;
+		}
+
+		if (needle[m] == '\0')
+		{
+			return (haystack + n);
+		}
+
+		n++;
+	}
+
+	return ('\0');
+}
+}
