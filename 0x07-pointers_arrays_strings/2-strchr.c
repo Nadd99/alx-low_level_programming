0435f3ff3638c9cacf0015b47a0d0772577ce12a
nad <its.nadd99@gmail.com>
2023-09-26 00:50:18 +0200

    created strchr function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,8 +11,8 @@
  */
 char *_strchr(char *s, char c)
 {
-	for(; *s != '\0'; s++)
+	for (; *s != '\0'; s++)
 		if (*s == c)
-			return (char *) s;
-	return NULL;
+			return ((char *) s);
+	return (NULL);
 }
