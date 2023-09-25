6c8abed3b2e65c34e7adfb8a426888ebcbdd552b
nad <its.nadd99@gmail.com>
2023-09-26 01:53:14 +0200

    commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,19 @@
 #include "main.h"
+/**
+ * print_chessboard - a function that prints chessboard
+ * @a: array of pieces
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int n, m;
+
+	for (n = 0; n < 8; n++)
+	{
+		for (m = 0; m < 8; m++)
+		{
+			_putchar(a[n][m]);
+		}
+		_putchar('\n');
+	}
+}
