6ed8ef41a87be7d242102fb6771f615f29f2c95a
nad <its.nadd99@gmail.com>
2023-09-26 00:21:02 +0200

    created strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr - locates a specific charecter in a string
+ *
+ * @s: the string to be scanned
+ * @c: the charecter to be located
+ *
+ * Return: a pointer to the first occurence of the charecter in the string
+ *         else NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int n = 0;
+	int m;
+
+	while (s[n])
+	{
+		n++;
+	}
+	for (m = 0; m <= n; m++)
+	{
+		if (c == s[m])
+		{
+			s = m++;
+			return (s);
+		}
+	}
+}
