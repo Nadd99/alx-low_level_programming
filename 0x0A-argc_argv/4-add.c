41484049449a0797556ebf3d71d5ff950dfcefbe
nad <its.nadd99@gmail.com>
2023-10-02 17:12:08 +0200

    commit changes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	{
 		printf("0\n");
 	}
-	else if (atoi(argv[i]) < 0 || atoi(argv[i]) > 9)
+	else if (atoi(argv[i]) < '0' || atoi(argv[i]) > '9')
 	{
 		printf("Error\n");
 		return (1);
