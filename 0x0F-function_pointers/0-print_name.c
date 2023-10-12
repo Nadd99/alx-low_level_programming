600f7bcaed874d0d2ebbd66f032456e290ae03a2
nad <its.nadd99@gmail.com>
2023-10-12 04:07:12 +0200

    commit changes

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: the name
+ * @f: a pointer to a function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
