824dcca2ff552513d69b82fde497e3c1abd30c22
Nadd99 <its.nadd99@gmail.com>
2023-10-20 04:33:19 +0200

    betty style

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -32,23 +32,20 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *last;
 
-   new = malloc(sizeof(list_t));
-   if (new == NULL)
-     return (NULL);
-
-   new->str = strdup(str);
-   new->len = _strlen(str);
-   new->next = NULL;
-
-   if (*head == NULL)
-     *head = new;
-   else
-   {
-     last = *head;
-     while (last->next != NULL)
-       last = last->next;
-     last->next = new;
-   }
-
-   return (*head);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+	return (*head);
 }
