14108a43d72332bccad1b60bd7fb57a0cdee6ec2
Nadd99 <its.nadd99@gmail.com>
2023-10-26 20:46:53 +0200

    one

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - ...
+ * @n: ....
+ * Return: ...
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0) {
+		_putchar('0');
+		return;
+	}
+	print_binary(n >> 1);
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
